70b85b6118c677d488a22b53f50cb36d9a5f1fcb
Zaragg <lcachao@hotmail.com>
2024-02-04 21:04:29 +0200

    Add channels

--- client/src/Me.jsx
+++ client/src/Me.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ServerList from "./components/ServerList";
 import Chat from "./components/Chat";
+import Channels from "./components/Channels";
 export default function () {
   return (
     <div className="me-page">
       <ServerList />
+      <Channels />
       <Chat />
     </div>
   );
--- client/src/components/channels.jsx
+++ client/src/components/channels.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import { React, useState } from "react";
 
 export default function channels() {
-  return <div>channels</div>;
+  const [selectedChannel, setSelectedChannel] = useState();
+  return (
+    <div className="channel-list">
+      <div className="channel">
+        <box-icon name="hash" color="#949ba4"></box-icon>
+        <div className="channel-name">general</div>
+      </div>
+    </div>
+  );
 }
