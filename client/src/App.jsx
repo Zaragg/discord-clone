fb9247fe21931101b7ccc488aacd012a987f05e3
NadaAlinour <lime.x.na@gmail.com>
2024-02-09 14:54:20 +0200

    Add registration form field errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import "./assets/stylesheets/form.css";
 import "./assets/stylesheets/mainpage.css";
 import "./assets/stylesheets/dropdown.css";
-import Dropdown from "./components/Dropdown";
 
 function App() {
   return (
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -15,6 +15,24 @@ export default function Register() {
     },
   });
 
+  const [emailErr, setEmailErr] = useState(false);
+  const [usernameErr, setUsernameErr] = useState(false);
+  const [passwordErr, setPasswordErr] = useState(false);
+
+  const [yearErr, setYearErr] = useState(false);
+  const [dayErr, setDayErr] = useState(false);
+  const [monthErr, setMonthErr] = useState(false);
+
+  useEffect(() => {
+    const updateDobErr = () => {
+      if (formData.dob.day != "") setDayErr(false);
+      if (formData.dob.month != "") setMonthErr(false);
+      if (formData.dob.year != "") setYearErr(false);
+    };
+    console.log("dob change");
+    updateDobErr();
+  }, [formData.dob]);
+
   const updateMonth = (item) => {
     console.log("update the state for the month");
     setFormData({
@@ -46,12 +64,41 @@ export default function Register() {
     });
   };
 
-  useEffect(() => {
-    console.log(formData)
-  }, [formData])
+  const validate = () => {
+    console.log(formData);
+    if (
+      formData.email == "" ||
+      formData.username == "" ||
+      formData.password == "" ||
+      formData.dob.month == "" ||
+      formData.dob.year == "" ||
+      formData.dob.day == ""
+    )
+      return false;
+    return true;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (formData.dob.month == "") setMonthErr(true);
+    if (formData.dob.day == "") setDayErr(true);
+    if (formData.dob.year == "") setYearErr(true);
+    if (formData.email == "") setEmailErr(true);
+    if (formData.username == "") setUsernameErr(true);
+    if (formData.password == "") setPasswordErr(true);
+
+    // clear errors messages
+    var isValid = validate();
+    console.log(isValid);
+
+    if (!isValid) console.log("client side errors. cannot proceed");
+
+    /*setEmailErr(false);
+    setUsernameErr(false);
+    setPasswordErr(false);
+    setMonthErr(false);
+    setDayErr(false);
+    setYearErr(false);*/
   };
   return (
     <div className="form-container">
@@ -63,14 +110,17 @@ export default function Register() {
 
           <div className="login-form-fields">
             <div className="form-input-field">
-              <label className="required">
-                <b>EMAIL</b>
+              <label className={emailErr ? "" : "required"}>
+                <span className={emailErr ? "error-span" : ""}>
+                  <b>EMAIL</b> {emailErr ? <i> - Required</i> : ""}
+                </span>
               </label>
               <input
                 type="text"
                 value={formData.email}
                 onChange={(e) => {
                   setFormData({ ...formData, email: e.target.value });
+                  !e.target.value ? setEmailErr(true) : setEmailErr(false);
                 }}
               />
             </div>
@@ -89,34 +139,51 @@ export default function Register() {
             </div>
 
             <div className="form-input-field">
-              <label className="required">
-                <b>USERNAME</b>
+              <label className={usernameErr ? "" : "required"}>
+                <span className={usernameErr ? "error-span" : ""}>
+                  <b>USERNAME</b> {usernameErr ? <i> - Required</i> : ""}
+                </span>
               </label>
               <input
                 type="text"
                 value={formData.username}
                 onChange={(e) => {
                   setFormData({ ...formData, username: e.target.value });
+                  !e.target.value
+                    ? setUsernameErr(true)
+                    : setUsernameErr(false);
                 }}
               />
             </div>
 
             <div className="form-input-field">
-              <label className="required">
-                <b>PASSWORD</b>
+              <label className={passwordErr ? "" : "required"}>
+                <span className={passwordErr ? "error-span" : ""}>
+                  <b>PASSWORD</b> {passwordErr ? <i> - Required</i> : ""}
+                </span>
               </label>
               <input
                 type="password"
                 value={formData.password}
                 onChange={(e) => {
                   setFormData({ ...formData, password: e.target.value });
+                  !e.target.value
+                    ? setPasswordErr(true)
+                    : setPasswordErr(false);
                 }}
               />
             </div>
 
             <div className="form-input-field">
-              <label className="required">
-                <b>DATE OF BIRTH</b>
+              <label
+                className={yearErr || monthErr || dayErr ? "" : "required"}
+              >
+                <span
+                  className={yearErr || monthErr || dayErr ? "error-span" : ""}
+                >
+                  <b>DATE OF BIRTH</b>{" "}
+                  {yearErr || monthErr || dayErr ? <i> - Required</i> : ""}
+                </span>
               </label>
               <div className="dob-register-container">
                 <Dropdown placeholder="Month" updateDOB={updateMonth} />
